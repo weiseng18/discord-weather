a149a71002229fbcf29679503d118aaf9b3b65ac
weiseng18 <tws.hci@gmail.com>
2021-03-07 15:02:30 +0800

    feat: add check

--- bot.py
+++ bot.py
@@ -28,4 +28,43 @@ client = commands.Bot(command_prefix = prefix)
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+# ---------------
+# bot commands
+# ---------------
+
+@client.command(
+	help="placeholder",
+	brief="placeholder"
+)
+async def check(ctx):
+	# pull data from data.gov.sg
+	json = getJSON(URL_RAIN)
+
+	# put data into meaningful variables
+	stationInfo = json["metadata"]["stations"]
+	UNIT = json["metadata"]["reading_unit"]
+	readings = json["items"][0]["readings"]
+	LAST_UPDATE = json["items"][0]["timestamp"]
+
+	# sort stations by distance from location specified in .env
+	P = {"latitude": float(ADDR_LAT), "longitude": float(ADDR_LONG)}
+	sortedStationInfo = distances(stationInfo, P)
+
+	# get 3 closest stations
+	closestStations = sortedStationInfo[:3]
+
+	# print last update timestamp
+	output = "Last updated at: {0}".format(LAST_UPDATE)
+	await ctx.channel.send(output)
+
+	# get rainfall info
+	for station in closestStations:
+		ID = station["id"]
+		NAME = station["name"]
+		DISTANCE = station["distance"]
+		RAINFALL = [reading for reading in readings if reading.get("station_id") == ID][0]["value"]
+
+		output = "Station at {0}, {1:.2f} km away, has reading {2} {3}".format(NAME, DISTANCE, RAINFALL, UNIT)
+		await ctx.channel.send(output)
+
 client.run(TOKEN)

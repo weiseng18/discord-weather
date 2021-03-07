490a905eb3bc92e8e14deaa50b545e648f763648
weiseng18 <tws.hci@gmail.com>
2021-03-07 15:01:04 +0800

    feat: create haversine() to get distance between two points in latitude and longitude

--- helper.py
+++ helper.py
@@ -4,6 +4,19 @@ def getJSON(URL):
 	json = get(URL).json()
 	return json
 
+# distance between two points, given latitudes and longitudes
+def haversine(A, B):
+	from math import sin, cos, asin, sqrt
+	R = 6371
+	diffLat = deg2rad(B["latitude"] - A["latitude"])
+	diffLong = deg2rad(B["longitude"] - A["longitude"])
+	
+	h = sin(diffLat / 2) ** 2 + cos(deg2rad(A["latitude"])) * cos(deg2rad(B["latitude"])) * sin(diffLong / 2) ** 2
+
+	d = 2 * R * asin(sqrt(h))
+
+	return d
+
 # convert degrees to radians
 def deg2rad(deg):
 	from math import pi
